126b04814c34af743dcebd1b0486ef24d3946766
modeste90 <modeste90@github.com>
2017-09-23 03:53:15 +0000

    recherche&vote
    Specifier la cetagorie de la recherche, puis une petite modification du AjaxStarratingsystemController

--- src/Ecommerce/EcommerceBundle/Controller/ProduitsController.php
+++ src/Ecommerce/EcommerceBundle/Controller/ProduitsController.php
@@ -6,9 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Ecommerce\EcommerceBundle\Form\RechercheType;
 use Ecommerce\EcommerceBundle\Entity\Categories;
 use Ecommerce\EcommerceBundle\Entity\SubCategories;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Ecommerce\EcommerceBundle\Entity\Media;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\Encoder\XmlEncoder;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
@@ -22,15 +23,15 @@ class ProduitsController extends Controller
         $session = $request->getSession();
         $em = $this->getDoctrine()->getManager();
 
-        if ($subCategorie != null){
-            $query = $em->createQuery("SELECT p FROM Ecommerce\EcommerceBundle\Entity\Produits p where p.quantite > 0 AND p.subCategorie = :subCategorie")
-                ->setParameters(['subCategorie' => $subCategorie,]);
+        /*var_dump($subCategorie);
+        die();*/
 
+        if ($subCategorie != null)
+            $findProduits = $em->getRepository('EcommerceBundle:Produits')
+                ->bySubCategorie($subCategorie);
+        else {
+            $query = $em->createQuery('SELECT p FROM Ecommerce\EcommerceBundle\Entity\Produits p where p.quantite > 0');
             $findProduits = $query->getResult();
-        }else {
-            $query = $em->createQuery('SELECT p FROM Ecommerce\EcommerceBundle\Entity\Produits p where p.quantite > 0')
-                ->setMaxResults(6);
-            $produits = $query->getResult();
         }
 
         if ($session->has('panier'))
@@ -38,10 +39,11 @@ class ProduitsController extends Controller
         else
             $panier = false;
 
+        $produits = $this->get('knp_paginator')->paginate($findProduits,$this->get('request')->query->get('page', 1),9);
         $rayons = $em->getRepository('EcommerceBundle:Rayon')->findAll();
+        $partenaires = $em->getRepository('EcommerceBundle:Partenaires')->findAll();
 
         if($subCategorie == null) {
-            $partenaires = $em->getRepository('EcommerceBundle:Partenaires')->findAll();
             return $this->render('EcommerceBundle:Default:produits/layout/produits.html.twig',
                 array(
                     'produits' => $produits,
@@ -50,7 +52,6 @@ class ProduitsController extends Controller
                     'partenaires' => $partenaires));
         }
         else{
-            $produits = $this->get('knp_paginator')->paginate($findProduits,$this->get('request')->query->get('page', 1),9);
             $query = $em->createQuery("SELECT cat FROM Ecommerce\EcommerceBundle\Entity\Categories cat where :subCategorie in ('cat.subCategories')")
                 ->setParameters(['subCategorie' => $subCategorie,]);
             $cat = $query->getResult();
@@ -62,36 +63,6 @@ class ProduitsController extends Controller
                     'panier' => $panier,
                     'rayons' => $rayons));
         }
-    }
-
-    public function plusAction()
-    {
-        $request = $this->container->get('request_stack')->getCurrentRequest();
-        $session = $request->getSession();
-        $em = $this->getDoctrine()->getManager();
-
-        $query = $em->createQuery('SELECT p FROM Ecommerce\EcommerceBundle\Entity\Produits p where p.quantite > 0');
-        $findProduits = $query->getResult();
-
-        if ($session->has('panier'))
-            $panier = $session->get('panier');
-        else
-            $panier = false;
-
-        $produits = $this->get('knp_paginator')->paginate($findProduits,$this->get('request')->query->get('page', 1),9);
-        $rayons = $em->getRepository('EcommerceBundle:Rayon')->findAll();
-        $query = $em->createQuery('SELECT p FROM Ecommerce\EcommerceBundle\Entity\Categories p ')
-            ->setMaxResults(1);
-        $cat = $query->getResult();
-        //$cat = $em->getRepository('EcommerceBundle:Categories')->getOneOrNullResult();
-
-        return $this->render('EcommerceBundle:Default:produits/layout/articles.html.twig',
-            array(
-                'cat' => $cat,
-                'produits' => $produits,
-                'panier' => $panier,
-                'rayons' => $rayons));
-
     }
 
     public function articlesAction(Categories $categorie)
@@ -123,8 +94,37 @@ class ProduitsController extends Controller
                 'rayons' => $rayons));
     }
 
+    /*public function produitsAction()
+    {
+        //$session = $this->getRequest()->getSession();
+        $request = $this->container->get('request_stack')->getCurrentRequest();
+        $session = $request->getSession();
+        $em = $this->getDoctrine()->getManager();
+
+        $query = $em->createQuery('SELECT p FROM Ecommerce\EcommerceBundle\Entity\Produits p where p.quantite > 0');
+        $findProduits = $query->getResult();
+        
+        if ($session->has('panier'))
+            $panier = $session->get('panier');
+        else
+            $panier = false;
+        
+        $produits = $this->get('knp_paginator')->paginate($findProduits,$this->get('request')->query->get('page', 1),9);
+        $rayons = $em->getRepository('EcommerceBundle:Rayon')->findAll();
+        $partenaires = $em->getRepository('EcommerceBundle:Partenaires')->findAll();
+        //$product = $repository->findOneByName('Keyboard'); // Pour les images
+
+        return $this->render('EcommerceBundle:Default:produits/layout/produits.html.twig',
+                            array(
+                                    'produits' => $produits,
+                                    'panier' => $panier,
+                                    'rayons' => $rayons,
+                                    'partenaires' => $partenaires));
+    }*/
+
     public function presentationAction($id)
     {
+        //$session = $this->getRequest()->getSession();
         $request = $this->container->get('request_stack')->getCurrentRequest();
         $session = $request->getSession();
         $em = $this->getDoctrine()->getManager();
@@ -148,33 +148,116 @@ class ProduitsController extends Controller
         $form = $this->createForm(new RechercheType());
         return $this->render('EcommerceBundle:Default:Recherche/modulesUsed/recherche.html.twig', array('form' => $form->createView()));
     }
-
-    public function rechercheTraitementAction()
+    
+    //recherche ajax
+    public function rechercheTraitementAction() 
     {
-
+        
         $request = $this->getRequest();
         if ($request->isXmlHttpRequest()) {
             $chaine = $request->get('chaine');
+            $subCategorie = $request->get('subCategorie'); 
+            //var_dump($subCategorie);
             $em = $this->getDoctrine()->getManager();
-            $produits = $em->getRepository('EcommerceBundle:Produits')->recherche($chaine);
-
+            if ($subCategorie==null) {
+                $produits = $em->getRepository('EcommerceBundle:Produits')->recherche($chaine);
+            }
+            else{
+                $produits = $em->getRepository('EcommerceBundle:Produits')
+                ->rechercheCategorie($chaine, $subCategorie);
+            }
+            
         }
 
         else {
             throw $this->createNotFoundException('La page n\'existe pas.');
         }
 
-
+        
         $response = new JsonResponse();
         if ($produits) {
             $prods = array();
             foreach ($produits as $produit) {
-                $prods[] = array("id" => $produit->getId(),
-                    "nom" => $produit->getNom());
+                $prods[] = array("id" => $produit->getId(), 
+                                 "nom" => $produit->getNom());
             }
-
+           
             $response->setData(array('produits' => $prods));
         }
+        else{
+            $response->setData(array('message' => 'Aucun résultat'));
+        }
+       
         return $response;
     }
+
+    /*public function rechercheTraitementAction() 
+    {
+        $form = $this->createForm(new RechercheType());
+        
+        if ($this->get('request')->getMethod() == 'POST')
+        {
+            $form->bind($this->get('request'));
+            $em = $this->getDoctrine()->getManager();
+            $produits = $em->getRepository('EcommerceBundle:Produits')->recherche($form['recherche']->getData());
+        } else {
+            throw $this->createNotFoundException('La page n\'existe pas.');
+        }
+        
+        return $this->render('EcommerceBundle:Default:produits/layout/produits.html.twig', array('produits' => $produits));
+    }*/
+
+    /*public function findMediaAction($name)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $media = new Media();
+        $query = $em->createQuery('SELECT i FROM Ecommerce\EcommerceBundle\Entity\Media i where i.name = $name');
+        $media = $query->getResult();
+        return $media;
+    }*/
+
+    /*public function getCommentsForSingleProduct(Product $product)
+    {
+        $qb = $this->createQueryBuilder('c')
+            ->where('c.product = :productId')
+            ->setParameter('productId', $product->getId());
+        $query = $qb->getQuery();
+        return $query->execute();
+    }*/
+
+    /*public function ajouterPanierAction($id)
+    {
+        //$session = $this->getRequest()->getSession();
+        
+        $request = $this->container->get('request_stack')->getCurrentRequest();
+        $session = $request->getSession();
+
+        if (!$session->has('panier')) $session->set('panier',array());
+        $panier = $session->get('panier');
+
+        if (array_key_exists($id, $panier)) {
+            if ($request->query->get('qte') != null) $panier[$id] = $request->query->get('qte');
+            $this->get('session')->getFlashBag()->add('success','Quantité modifié avec succès');
+        } else {
+            if ($request->query->get('qte') != null)
+                $panier[$id] = $request->query->get('qte');
+            else
+                $panier[$id] = 1;
+
+            $this->get('session')->getFlashBag()->add('success','Article ajouté avec succès');
+        }
+
+        $session->set('panier',$panier);
+
+        $response = new JsonResponse();
+        $response->setData(array(
+            'articles' => count($session->get('panier')),
+            'message' => 'Quantité modifié avec succès'
+        ));
+
+        //return $this->render(controller('EcommerceBundle:Panier:menu'));
+
+        //return $this->redirect($request->headers->get('referer'));
+        return $response;
+    }*/
 }
