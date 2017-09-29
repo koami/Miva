4ea110a746e670126227cc3c92a63b8edc845d2b
Koami <van.romy8@gmail.com>
2017-09-29 15:37:09 +0000

    Trying Fix app

--- web/app.php
+++ web/app.php
@@ -15,10 +15,11 @@ $apcLoader->register(true);
 
 
 /* Pour Debugger quand le app.php is blank
- * ini_set('display_errors', 1);
+ */
+ini_set('display_errors', 1);
 error_reporting(-1);
 ini_set('display_errors', 1);
-error_reporting(-1);*/
+error_reporting(-1);
 
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
