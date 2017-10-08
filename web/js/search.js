260e6003f4d28c2316211c2f148f8e13513a6e5a
modeste90 <modeste90@github.com>
2017-10-08 05:18:25 +0000

    Redirection+dynamisme panier
    -rendre dynamique le panier
    -panierController
    -presentation.html.twig(AjouterAction n'est plus utiliser)
    -panier.html.twig pour ajout du boutton vider le panier
    -panierRouting
    -search.js
    -login.html.twig pour la redirection

--- web/js/search.js
+++ web/js/search.js
@@ -4,9 +4,7 @@ $('document').ready(function(){
 		if (motCle.length >= 1) {
 			$('.loading').remove();
 			var subCategorie = $('.subCategorie').val();
-			console.log(subCategorie);
 			data = {chaine: motCle, subCategorie: subCategorie};
-			console.log(data);
 			var path = $('.formAjax').attr('data-action');
 			$.ajax({
 				type: 'get',
@@ -15,23 +13,26 @@ $('document').ready(function(){
 				dataType: 'json',
 				beforeSend: function(){
 					$("form .searchAjax").parent().append('<div class="loading"></div>');
-					//console.log('ok');
 				},
 				success: function(data){
-					//console.log(data);
-					console.log(ROOT_URL);
 					$(".aja").empty();
 					if (data.message) {
 						$(".aja").append("<li><a href='#'>" +data.message+ "</a></li>");
 					}
-					$.each(data.produits, function(index, produit) {
-						$(".aja").append("<li><a href="+ROOT_URL+"produit/"+produit.id+ ">" +produit.nom+ "</a></li>");
-                   });
+					else{
+						$.each(data.produits, function(index, produit) {
+							$(".aja").append("<li><a href="+ROOT_URL+"produit/"+produit.id+ ">" +produit.nom+ "</a></li>");
+                   		});
+					}
+					
 					$('.loading').remove();
 					
 				}
 			});
 			return false;
+		}
+		else{
+			$(".aja").empty();
 		}
 		
 	});
