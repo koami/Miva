c375daa9b90a7ae6fb6ffc87e3b130edbaf28a74
Koami <van.romy8@gmail.com>
2017-11-18 04:27:30 +0000

    list

---
+++ web/js/admin/clients.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Koami on 18/11/2017.
+ */
+var options = {
+    valueNames: ['nom']
+};
+
+var usersList = new List('test-list', options,{
+    page: 10,
+    pagination: true
+});
---
+++ web/js/admin/garantie.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Koami on 18/11/2017.
+ */
